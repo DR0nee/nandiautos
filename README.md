f1844f5a8fa19db9f957fe6c3a9c2ad1aee53d12
Kristof <tkristof88@gmail.com>
2018-04-12 13:50:33 +0100

    Webpage ready do rock

---
+++ README.md
@@ -0,0 +1,5 @@
+# nandiautos
+
+This is my own project to my Friend father,
+Who needed a new website to his company. 
+"Car Breakdown Recovery & Auto Towing Service, Jump Start, Vehicle Recovery"
---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".frame").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    draggable: true,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
